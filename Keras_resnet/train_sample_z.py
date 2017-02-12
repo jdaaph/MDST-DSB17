35d93b0d5b40bab686a6bf9ccae14ce49557522b
Chengyu Dai <daich@flux-login2.arc-ts.umich.edu>
2017-02-12 15:04:24 -0500

    adding multiple loading and test on sample first, not working yet

--- Keras_resnet/train_sample_z.py
+++ Keras_resnet/train_sample_z.py
@@ -62,7 +62,7 @@ if not data_augmentation:
               nb_epoch=nb_epoch,
               validation_data=(X_test, Y_test),
               shuffle=True,
-              callbacks=[lr_reducer, csv_logger])
+              callbacks=[lr_reducer, early_stopper, csv_logger])
 else:
     print('Using real-time data augmentation.')
     # This will do preprocessing and realtime data augmentation:
@@ -87,4 +87,4 @@ else:
                         samples_per_epoch=X_train.shape[0],
                         validation_data=(X_test, Y_test),
                         nb_epoch=nb_epoch, verbose=1, max_q_size=100,
-                        callbacks=[lr_reducer, csv_logger])
+                        callbacks=[lr_reducer, early_stopper, csv_logger])
